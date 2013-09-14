1d67385df1d72bf71450625ffb0f701c501ecd60
gritbear20 <lx8@rice.edu>
2013-09-13 23:48:01 -0500

    fully functional flashing led at 1Hz

--- blinky.c
+++ blinky.c
@@ -8,12 +8,6 @@ void SysTick_Handler(void) {
   msTicks++;                                     /* increment timeTicks counter */
 }
 
-
-void nmi_handler(void);												
-void hardfault_handler(void);
-void delay(void);
-int main(void);
-
 /*	Four vectors - the starting stack pointer value, code entry point and NMI and Hard-Fault handlers */
 __INLINE static void Delay (uint32_t dlyTicks) {
   uint32_t curTicks = msTicks;
@@ -21,17 +15,12 @@ __INLINE static void Delay (uint32_t dlyTicks) {
   while ((msTicks - curTicks) < dlyTicks);
 }
 
-
-unsigned int * myvectors[4] 
-__attribute__ ((section("vectors")))= {
-    (unsigned int *)	STACK_TOP,         
-    (unsigned int *) 	main,                
-};
-
-
 int main(void){	
-	int n = 0;
-	int button;
+	 if (SysTick_Config (SystemCoreClock / 1000)) { /* Setup SysTick for 1 msec interrupts */
+    ;                                            /* Handle Error */
+    while (1);
+  }
+  
 
 	RCC->APB2ENR |= 0x10 | 0x04 | 0x08;/* Enable the GPIOA (bit 2) and GPIOC (bit 8)  and GPIOB*/
 	GPIOB->CRL = 0x00100000;	//set GIPOB-5 to out
@@ -45,13 +34,6 @@ int main(void){
 		Delay (500);                                 /* delay  100 Msec    */
 		GPIOB->BSRR = 1<<5 ;
 		Delay (500);                                 /* delay  100 Msec    */
+		GPIOB->BSRR = 1<<21;
     }
-}
-
-void delay(void) 
-{
-	int i = 10000000;							/* About 1/4 second delay */
-	while (i-- > 0) {
-		asm("nop");						/* This stops it optimising code out */
-	}
 }
