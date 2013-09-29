19041374d5c8920e7b9deaf941aa1b87891cbb2e
gritbear20 <lx8@rice.edu>
2013-09-29 14:51:43 -0500

    buggy HSE and good HSI 64MHz

--- blinky.c
+++ blinky.c
@@ -4,28 +4,179 @@
 //define a counter for the delay 
 volatile uint32_t msTicks;
 
+
+void initHSI(){
+	uint32_t HSIStatus;
+	int StartUpCounter = 0;
+
+	/* Reset the RCC clock configuration to the default reset state(for debug purpose) */
+	/* Set HSION bit */
+	RCC->CR |= (uint32_t)0x00000001;
+ 
+	/* Reset SW, HPRE, PPRE1, PPRE2, ADCPRE and MCO bits */
+	RCC->CFGR &= (uint32_t)0xF8FF0000;
+ 
+	/* Reset HSEON, CSSON and PLLON bits */
+	RCC->CR &= (uint32_t)0xFEF6FFFF;
+ 
+	/* Reset HSEBYP bit */
+	RCC->CR &= (uint32_t)0xFFFBFFFF;
+	 
+	/* Reset PLLSRC, PLLXTPRE, PLLMUL and USBPRE/OTGFSPRE bits */
+	RCC->CFGR &= (uint32_t)0xFF80FFFF;
+	 
+	/* Disable all interrupts and clear pending bits  */
+	RCC->CIR = 0x009F0000;
+
+	RCC->CR |= ((uint32_t)RCC_CR_HSION);
+	 do {
+	 HSIStatus = RCC->CR & RCC_CR_HSIRDY;
+	 StartUpCounter++;
+	 } while((HSIStatus == 0) && (StartUpCounter != 0x0500));
+	 
+	 if ((RCC->CR & RCC_CR_HSIRDY) != RESET) {
+	 HSIStatus = (uint32_t)0x01;
+	 } else {
+	 HSIStatus = (uint32_t)0x00;
+	 }
+
+	if (HSIStatus == (uint32_t)0x01) {
+	 /* Enable Prefetch Buffer */
+	 FLASH->ACR |= FLASH_ACR_PRFTBE;
+	 
+	 /* Flash 2 wait state */
+	 FLASH->ACR &= (uint32_t)((uint32_t)~FLASH_ACR_LATENCY);
+	 FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_2;
+	 
+	 /* HCLK = SYSCLK */
+	 RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
+	 
+	 /* PCLK2 = HCLK */
+	 RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE2_DIV1;
+	 
+	 /* PCLK1 = HCLK */
+	 RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE1_DIV2;
+	 
+	 /*  PLL configuration: PLLCLK = HSI * 9 = 72 MHz */
+	 
+	 /* Set Bits to 0 */
+	 RCC->CFGR &= (uint32_t)((uint32_t)~(
+	  RCC_CFGR_PLLSRC   |
+	  RCC_CFGR_PLLXTPRE |
+	  RCC_CFGR_PLLMULL ));
+	 /* Set Bits to 1 */
+	 RCC->CFGR |= (uint32_t)(
+	  RCC_CFGR_PLLSRC_HSI_Div2 |
+	  RCC_CFGR_PLLMULL16 );
+	 
+	 /* Enable PLL */
+	 RCC->CR |= RCC_CR_PLLON;
+	 
+	 /* Wait till PLL is ready */
+	 while((RCC->CR & RCC_CR_PLLRDY) == 0)
+	 {
+	 }
+	 
+	 /* Select PLL as system clock source */
+	 RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
+	 RCC->CFGR |= (uint32_t)RCC_CFGR_SW_PLL;
+	 
+	 /* Wait till PLL is used as system clock source */
+	 while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != (uint32_t)0x08)
+	 {
+	 }
+	}
+	
+}
+
+
 //define the system init function
 void init(){
-	//to do: set RCC_CR  Clock control register
+	//pre-defined variables
+	uint32_t HSEStatusCheck;
+	int StartUpCounter = 0;
 
-	//to do: set RCC_CFGR Clock configuration register
+	//-----------------------------------------------------------------
+	/* Reset the RCC clock configuration to the default reset state(for debug purpose) */
+	/* Set HSEON(16) bit */
+	RCC->CR |= (uint32_t)0x00010001;
 
-	//to do: set RCC_CIR Clock interrupt register
+	/* Reset SW(1:0)->01: HSE is selected as system clock (for now), 
+	HPRE(7:4) AHB prescaler->0xxx: SYSCLK not divided to get HCLK
+	PRE1(10:8) APB low-speed prescaler (APB1)->100: HCLK divided by 2
+	PPRE2(13:11) APB high-speed prescaler (APB2)->0xx: HCLK not divided
+	ADCPRE(15:14) ADC prescaler -> 00, devided by 2
+	and MCO(26:24) Microcontroller clock output -> 100 (SYSCLK) */
+	RCC->CFGR &= (uint32_t)0xF8FF0801;
 
-	//to do: APB2 peripheral reset register (RCC_APB2RSTR) (maybe)
+	/* Reset HSION(0) -> 0, HSEON(16) -> 1, HSEBYP(18) -> 0, CSSON(19) -> 0 and PLLON(24) -> 0 bits */
+	RCC->CR &= (uint32_t)0xFEF3FFFE; 
+	 
+	/* Disable all interrupts and clear pending bits  */
+	RCC->CIR = 0x009F0000;
+	
+	//-----------------------------------------------------------------------------------------------
+	//enable HSE:
+	//wait until the HSEON is ready
+	RCC->CR |= ((uint32_t)RCC_CR_HSEON);
+	do {
+		 HSEStatusCheck = RCC->CR & RCC_CR_HSERDY;
+		 StartUpCounter++;
+	 } while((HSEStatusCheck == 0) && (StartUpCounter != 0x1000));
+	
+	if ((RCC->CR & RCC_CR_HSERDY) != RESET) {
+ 		HSEStatusCheck = (uint32_t)0x01;
+ 	} else {
+ 		HSEStatusCheck = (uint32_t)0x00;
+ 	}
 
-	//to do: APB1 peripheral reset register (RCC_APB1RSTR) (maybe)
-
-	//to do: AHB peripheral clock enable register (RCC_AHBENR) (maybe)
-
-	//to do: APB2 peripheral clock enable register (RCC_APB2ENR) (maybe)
-
-	//to do: APB1 peripheral clock enable register (RCC_APB1ENR)
-
-	//to do: Backup domain control register (RCC_BDCR)
-
-	//to do: Control/status register (RCC_CSR)
-
+	//----------------------------------------------------------------------------------------------
+	//enable PLL and set it as the system clock source
+	if (HSEStatusCheck == (uint32_t)0x01) {
+		/* Enable Prefetch Buffer */
+	 	FLASH->ACR |= FLASH_ACR_PRFTBE;
+	 
+	 	/* Flash 2 wait state */
+	 	FLASH->ACR &= (uint32_t)((uint32_t)~FLASH_ACR_LATENCY);
+	 	FLASH->ACR |= (uint32_t)FLASH_ACR_LATENCY_2;
+	 
+	 	/* HCLK = SYSCLK */
+	 	RCC->CFGR |= (uint32_t)RCC_CFGR_HPRE_DIV1;
+	 
+	 	/* PCLK2 = HCLK */
+	 	RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE2_DIV1;
+	 
+	 	/* PCLK1 = HCLK */
+	 	RCC->CFGR |= (uint32_t)RCC_CFGR_PPRE1_DIV2;
+	 
+	 	/*  PLL configuration: PLLCLK = HSI/2 * 9 = 36 MHz */
+	 
+	 	/* Set Bits to 0 */
+	 	/* Reset PLLSRC(16)->1, PLLXTPRE(17)->1, PLLMUL(21:18)-> 011 and USBPRE(22)->0 bits 
+		PLLSRC(16)->1: HSE oscillator clock selected as PLL input clock (16MHz)
+		PLLXTPRE(17)->1: divided HSE by 2 (16/2 = 8MHz), must be done before PLLON is set as 1
+		PLLMUL(21:18)-> 0111: times HSE by 9 {0xFF (11 01->D) (11 11->F)  FFFF}
+		USBPRE(22)->1: PLL clock is not divided
+		*/
+		RCC->CFGR &= (uint32_t)0xFFDFFFFF;
+	 
+	 	/* Enable PLL */
+	 	RCC->CR |= RCC_CR_PLLON;
+	 
+	 	/* Wait till PLL is ready */
+	 	while((RCC->CR & RCC_CR_PLLRDY) == 0)
+	 	{
+	 	}
+	 
+	 	/* Select PLL as system clock source */
+	 	RCC->CFGR &= (uint32_t)((uint32_t)~(RCC_CFGR_SW));
+	 	RCC->CFGR |= (uint32_t)RCC_CFGR_SW_PLL;
+	 
+	 	/* Wait till PLL is used as system clock source */
+	 	while ((RCC->CFGR & (uint32_t)RCC_CFGR_SWS) != (uint32_t)0x08)
+	 	{
+	 	}
+	}
 }
 
 
@@ -43,7 +194,7 @@ __INLINE static void Delay (uint32_t dlyTicks) {
 }
 
 int main(void){	
-	init();
+	initHSI();
 
 	if (SysTick_Config (SystemCoreClock / 1000)) {
         //Setup the system interrupt to once 1ms
